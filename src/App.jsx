d9d9f64aa11d4cbc84b8f800722bfb80926e9bfd
yassir5236 <yassiraitlahmadi@gmail.com>
2024-12-02 21:35:31 +0100

    start working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
-
+import { BrowserRouter } from "react-router-dom";
+import { About ,Contact,Navbar,Hero ,Tech ,Works , Experience ,Feedbacks , StarsCanvas} from "./components";
 const App = () => {
 
   return (
- 
-    <>
-    3D Developer
-    </>
+
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
